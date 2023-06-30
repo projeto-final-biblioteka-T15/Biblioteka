2537ee9a05ee6b8b5b095ba0373a84d0da647203
Arthur Silva Mendonça <arthu91@gmail.com>
2023-06-30 16:27:36 -0300

    feat: adicionado books.urls a biblioteka.urls

--- biblioteka/urls.py
+++ biblioteka/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("copies.urls")),
+    path("api/", include("books.urls")),
 ]
