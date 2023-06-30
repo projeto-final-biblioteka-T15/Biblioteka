b5570eb345b55c04fa87bf5b6f02c8ed44303faf
Arthur Silva Mendonça <arthu91@gmail.com>
2023-06-30 16:02:02 -0300

    feat: criado CopyDetailView

--- copies/urls.py
+++ copies/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import CopyView
+from .views import CopyView, CopyDetailView
 
-urlpatterns = [path("copies/", CopyView.as_view())]
+urlpatterns = [
+    path("copies/", CopyView.as_view()),
+    path("copies/<int:pk>/", CopyDetailView.as_view()),
+]
