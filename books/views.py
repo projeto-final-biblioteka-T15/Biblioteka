120f0482b1d2798916173e9437d8f04fe3c8510f
silviacarvalhodacunha <silviacarvalhodacunha@gmail.com>
2023-07-04 23:10:51 -0300

    fix: retorno da criação de books

--- books/views.py
+++ books/views.py
@@ -1,8 +1,6 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .serializers import BookSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .permissions import IsLibraryStaff
 
