c3c601f4d7fa9e9227683cddd31c65f83a2388c1
Arthur Silva Mendonça <arthu91@gmail.com>
2023-07-06 19:03:05 -0300

    feat: criado o sistema de seguir livros dentro de copies

--- copies/serializers.py
+++ copies/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Copies
 from books.models import Book
 from books.serializers import BookSerializer
+from books.models import Book
+from .models import BookFollower
+
+
+class BookFollowerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookFollower
+        fields = ["id", "user", "book"]
 
 
 class CopiesSerializer(serializers.ModelSerializer):
@@ -16,11 +24,18 @@ class CopiesSerializer(serializers.ModelSerializer):
         book = BookSerializer(instance.book).data
         representation["book"] = book
         return representation
-    
+
     def create(self, validated_data):
         book_id = validated_data.pop("book").id
-        book = Book.objects.get(id=book_id)        
+        book = Book.objects.get(id=book_id)
 
         copies = Copies.objects.create(book=book, **validated_data)
 
         return copies
+
+
+# para testar o envio de e-mail
+class SendEmailSerializer(serializers.Serializer):
+    subject = serializers.CharField()
+    message = serializers.CharField()
+    recipient_list = serializers.ListField()
