c26cc5a0cf9eacabdefc72f56ae0bebc3486fdbe
silviacarvalhodacunha <silviacarvalhodacunha@gmail.com>
2023-07-10 22:25:18 -0300

    feat: unfollow

--- copies/permissions.py
+++ copies/permissions.py
@@ -1,9 +1,16 @@
 from rest_framework import permissions
+from django.shortcuts import get_object_or_404
+from .models import Copies
+from .views import BookFollower
 
 
-class IsLibraryStaffOrAuthenticated(permissions.BasePermission):
+class IsLibraryStaffOrBookFollower(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated or request.method in permissions.SAFE_METHODS
-
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated and request.user.user_type == 'library_staff'
+        if request.user.is_authenticated and request.user.user_type == 'library_staff':
+            return True
+        elif request.method in ['GET'] and request.user.is_authenticated:
+            copy_id = view.kwargs.get('pk')
+            copy = get_object_or_404(Copies, pk=copy_id)
+            book_follower = BookFollower.objects.filter(user=request.user, book=copy.book).exists()
+            return book_follower
+        return False
--- copies/urls.py
+++ copies/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CopyView, CopyDetailView, BookFollowView
+from .views import CopyView, CopyDetailView, BookFollowView, BookListFollowersView, BookUnfollowView
 
 urlpatterns = [
     path("copies/", CopyView.as_view()),
     path("copies/<int:pk>/", CopyDetailView.as_view()),
     path("copies/<int:pk>/follow/", BookFollowView.as_view()),
+    path("copies/followers/", BookListFollowersView.as_view()),
+    path("copies/<int:pk>/unfollow/", BookUnfollowView.as_view()),
 ]
